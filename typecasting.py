d1a7b38d0f1baee15abd314c03f43a09384d619b
Rishik1507 <microsoftrishik@gmail.com>
2024-10-22 21:27:38 +0530

    c

---
+++ typecasting.py
@@ -0,0 +1,14 @@
+a=34
+b="Rishik"
+c=56.98
+d=True
+print("The type of a is:", type(a))
+
+print("The type of b is:", type(b))
+print("The type of c is:", type(c))
+print("The type of d is:", type(d))
+
+a=float(a)
+d=int(d)
+print(a)
+print(d)
